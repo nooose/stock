7bda9fb83d5e639d29f356d902f295a1b5668a9f
nooose <hd15807@gmail.com>
2023-03-21 00:03:23 +0900

    feat: 재고 수량 감소 기능 구현
    추가로 테스트 케이스를 작성했다.

---
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -0,0 +1,37 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class StockServiceTest {
+
+    @Autowired
+    private StockService stockService;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    void setUp() {
+        Stock stock = new Stock(1L, 100L);
+        stockRepository.saveAndFlush(stock);
+    }
+
+    @DisplayName("재고 감소")
+    @Test
+    void decreaseStock() {
+        stockService.decrease(1L, 1L);
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        assertThat(stock.getQuantity()).isEqualTo(99);
+    }
+}
